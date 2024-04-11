631bdf26b2f7e98d31e78bb603ce121d9bc40ffa
Flaradr <matthieugayraud@gmail.com>
2024-04-11 11:38:52 +0200

    Minor refactoring on hexadecimal converter and add test

--- src/main/java/util/HexadecimalConverter.java
+++ src/main/java/util/HexadecimalConverter.java
@@ -1,6 +1,6 @@
 package util;
 
-public class HexaConverter {
+public class HexadecimalConverter {
 
 
     /**
@@ -9,7 +9,7 @@ public class HexaConverter {
      * @param hex Value in hexadecimal
      * @return integer value of the hexadecimal read in little endian
      */
-    public static int hexToIntLittleEndian(String hex) throws NumberFormatException {
+    public static int hexadecimalToIntLittleEndian(String hex) throws NumberFormatException {
         StringBuilder res = new StringBuilder();
         String[] bytes = hex.split("(?<=\\G.{2})"); //Positive lookbehind
         for (String str : bytes) {
@@ -20,14 +20,30 @@ public class HexaConverter {
 
     /**
      * Convert an array of bytes to a String containing the hexadecimal value.
+     *
      * @param bytes Bytes to convert
      * @return Hexadecimal value of the bytes array
      */
-    public static String bytesToHexa(byte[] bytes) {
+    public static String bytesToHexadecimal(byte[] bytes) {
         StringBuilder hexValue = new StringBuilder();
         for (byte aByte : bytes) {
             hexValue.append(String.format("%02X", aByte));
         }
         return hexValue.toString();
+    }
+
+    /**
+     * Transform hexadecimal value to its ascii value
+     * @param hexStr
+     * @return String of ASCII
+     */
+    public static String hexToAscii(String hexStr) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < hexStr.length(); i += 2) {
+            String str = hexStr.substring(i, i + 2);
+            output.append((char) Integer.parseInt(str, 16));
+        }
+
+        return output.toString();
     }
 }
