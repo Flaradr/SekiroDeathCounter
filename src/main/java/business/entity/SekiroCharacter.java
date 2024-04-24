583f8332fa627d5a401f29b372950b2de5a19ae6
Flaradr <matthieugayraud@gmail.com>
2024-04-24 13:54:56 +0200

    Add html display for elden ring and sekiro

--- src/main/java/business/entity/SekiroCharacter.java
+++ src/main/java/business/entity/SekiroCharacter.java
@@ -1,4 +1,14 @@
 package business.entity;
 
-public class SekiroCharacter extends FromSoftwareCharacter{
+public class SekiroCharacter extends FromSoftwareCharacter {
+
+    @Override
+    public String stringifiesToHtml() {
+        return "<html>\n" +
+                "<ul>\n" +
+                "<li>Nom : " + super.getCharacterName() + "</font>\n" +
+                "<li>Nombre de mort : <font color=red>" + super.getDeathCount() + "</font>\n" +
+                "</ul>\n" +
+                "</html>";
+    }
 }
